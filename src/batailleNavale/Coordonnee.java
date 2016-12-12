39a8248e7384746310c86978a1271bdd61745ec1
Hiyuu <Hiyuu@130.190.0.192>
2016-12-12 16:01:02 +0100

    // Coordonnées : CompareTo Object à terminer

--- src/batailleNavale/Coordonnee.java
+++ src/batailleNavale/Coordonnee.java
@@ -1,36 +1,122 @@
+/**
+ * Titre : Bataille Navale Description : TP stage de programmation Java
+ * Copyright : Copyright (c) 2016 Christophe Brioua, Andy Dowlut, Michael Toth
+ * Realise dans le cadre du Master DCISS de l'Universite Grenoble-Alpes
+ *
+ * @author Christophe Brioua, Andy Dowlut, Michael Toth
+ * @version 1.0
+ */
 package batailleNavale;
 
 public class Coordonnee {
-	
-	/*Une instance de la classe Coordonnee représente une coordonnee dans une grille de bataille
-	navale. Cette classe doit en particulier permettre de realiser facilement des conversions entre le
-	systeme de coordonnees utilise dans la bataille navale, où les colonnes sont notées par des lettres
-	majuscules (de 'A' à 'Z') et les lignes par des nombres (de 1 à 26), et les indices utilises en Java
-	(de 0 à 25).*/
+        private int ligne;
+        private int colonne;
 
-	private int ligne;
-	private int colonne;
-	
-	public Coordonnee(int ligne, int colonne) {
-		this.ligne = ligne;
-		this.colonne = colonne;
-	}
-	
-	// public Coordonee(String s) {}
-	// public String toString() {}
-	public int getLigne() {
-		return this.ligne;
-	}
-	public int getColonne() {
-		return this.colonne;
-	}
-	// public boolean equals(Object obj) {}
-	// public boolean voisine(Coordonnee c) {}
-	
-	
-	//// Main pour terster les différentes méthodes	////
-	public static void main(String[] args) {
-		System.out.println("coucou le monde bis !");
-	}
+        /**
+         * Une instance de la classe Coordonnee represente une coordonnee dans une
+         * grille de bataille navale. Cette classe doit en particulier permettre de
+         * realiser facilement des conversions entre le systeme de coordonnees
+         * utilise dans la bataille navale, ou les colonnes sont notees par des
+         * lettres majuscules (de 'A' a 'Z') et les lignes par des nombres (de 1 a
+         * 26), et les indices utilises en Java (de 0 a 25).
+         */
+
+        public Coordonnee(int ligne, int colonne) {
+
+                // Permet d'obtenir une coordonnee de ligne i et de colonne j (indices
+                // Java)
+
+                ligne = this.ligne;
+                colonne = this.colonne;
+        }
+
+        public Coordonnee(String s) {
+
+                // Permet d'obtenir une coordonnee d'apres son expression donnee par s
+                // dans le systeme de coordonnees de la bataille navale
+
+                this.colonne = Integer.parseInt(s.substring(0, 1));
+                this.ligne = Integer.parseInt(s.substring(1));
+                if (ligne < 0 || colonne < 0 || ligne > 25 || colonne > 25)
+                        throw new IllegalArgumentException("Valeur incorrecte");
+                // exception si les coordonnees sont incorrectes (incompletes, etc.)
+        }
+
+        public String toString() {
+
+                // Retourne une String exprimant this dans le systeme de coordonnee de
+                // la bataille navale (exemple : "C6")
+
+                String s = "";
+                for (int i = 0; i < this.colonne; i++) {
+                        for (int j = 0; j < this.ligne; j++) {
+                                s += this.colonne + ", " + this.ligne + " ";
+                        }
+                        s += " \n";
+                }
+                return s;
+        }
+
+        public int getLigne() {
+
+                // Accesseur en lecture pour l'indice de ligne
+
+                return this.ligne;
+        }
+
+        public int getColonne() {
+
+                // Accesseur en lecture pour l'indice de colonne
+
+                return this.colonne;
+        }
+
+        public boolean equals(Object obj) {
+
+                // Retourne true si et seulement si this est equivalent a obj
+
+//              if (!(obj instanceof Coordonnee))
+//                      return false;
+//              Coordonnee c = (Coordonnee) obj;
+//              if (this.ligne != Coordonnee.ligne || this.colonne != Coordonnee.colonne)
+//                      return false;
+                return true;
+        }
+
+        public boolean voisine(Coordonnee c) {
+
+                // Retourne true si et seulement si this est une coordonnee voisine
+                // (verticalement ou horizontalement) de c
+
+                if (this.ligne - c.ligne == 1 || this.ligne - c.ligne == -1 || this.colonne - c.colonne == 1 || this.colonne - c.colonne == -1)
+                        return true;
+                return false;
+        }
+
+        public int compareTo(Coordonnee c) {
+
+                // Retourne le resultat de la comparaison de this et de c. Une
+                // coordonnee est consideree inferieure a une autre, si elle se trouve
+                // sur une ligne inferieure ou si elle se trouve sur la meme ligne mais
+                // sur une colonne inferieure
+        		
+                if(this.ligne < c.ligne)
+                	return -1;
+                else if(this.ligne > c.ligne)
+                	return 1;
+                
+                else if(this.ligne == c.ligne)
+                		if(this.colonne == c.colonne)
+                			return 0;
+                		else if(this.colonne < c.colonne)
+                			return -1;
+                		else
+                			return 1;                
+        }
+
+        public static void main(String[] args) {
+                // TODO Auto-generated method stub
+
+        }
 
 }
