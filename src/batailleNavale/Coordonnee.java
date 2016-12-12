a58d2e78caf1d7ddaf01c24848f659f40b14a576
Hiyuu <Hiyuu@130.190.0.192>
2016-12-12 13:35:52 +0100

    // First change (try)

--- src/batailleNavale/Coordonnee.java
+++ src/batailleNavale/Coordonnee.java
@@ -3,7 +3,7 @@ package batailleNavale;
 public class Coordonnee {
 
 	public static void main(String[] args) {
-		System.out.println("coucou le monde !");
+		System.out.println("coucou le monde bis !");
 	}
 
 }
