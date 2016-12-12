f4cf10312946606a1538b50c02d63660f391d63d
Hiyuu <Hiyuu@130.190.0.192>
2016-12-12 14:00:52 +0100

    // Ajout de la classe Navire.java

--- src/batailleNavale/Coordonnee.java
+++ src/batailleNavale/Coordonnee.java
@@ -1,7 +1,26 @@
 package batailleNavale;
 
 public class Coordonnee {
+	
+	/*Une instance de la classe Coordonnee représente une coordonnée dans une grille de bataille
+	navale. Cette classe doit en particulier permettre de réaliser facilement des conversions entre le
+	système de coordonnées utilisé dans la bataille navale, où les colonnes sont notées par des lettres
+	majuscules (de 'A' à 'Z') et les lignes par des nombres (de 1 à 26), et les indices utilisés en Java
+	(de 0 à 25).*/
 
+	private int ligne;
+	private int colonne;
+	
+	// public Coordonnee(int ligne, int colonne) {}
+	// public Coordonee(String s) {}
+	// public String toString() {}
+	// public int getLigne() {}
+	// public int getColonne() {}
+	// public boolean equals(Object obj) {}
+	// public boolean voisine(Coordonnee c) {}
+	
+	
+	//// Maint pour terster les différentes méthodes	////
 	public static void main(String[] args) {
 		System.out.println("coucou le monde bis !");
 	}
