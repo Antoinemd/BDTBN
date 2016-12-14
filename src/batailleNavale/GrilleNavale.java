a4a223aebc4a1636c751b5972b9b735fdbfd5c03
Hiyuu <Hiyuu@130.190.3.234>
2016-12-14 14:01:16 +0100

    // Màj 14/12/16 à 14:01

--- src/batailleNavale/GrilleNavale.java
+++ src/batailleNavale/GrilleNavale.java
@@ -35,32 +35,35 @@ public class GrilleNavale {
 	}
 
 	// 3.2 : Méthodes
-	public String toString() { // Andy : je pars du principe que la grille est
-								// carrée
-		String map = " ";
+	public String toString() { // Andy : je pars du principe que la grille est carrée
+		String map = "";
 		char c = 'A';
 		char n = '0';
-		for (int i = 0; i <= this.taille; i++) {
-			for (int j = 0; j < this.taille; j++) {
-
-				if (j == 0) { // si on est au début de la ligne, on met le
-								// numéro de la ligne
-					if (n == '0') // mais on n'affiche pas le zéro
-						map += "";
-					else
-						map += n;
+		for (int i = 0; i < this.taille + 1; i++) {		// on avance sur les lignes
+			for (int j = 0; j < this.taille + 1; j++) {		// on avance sur les colonnes
+				
+				// Première colonne : on liste les chiffres
+				if(j == 0) {
+					if(n == '0') {	// On n'affiche pas le 0
+						map += " " + " ";
+						n++;
+					}
+					else {
+					map += n + " ";	// Mais on affiche les autres chiffres !
 					n++;
+					}
 				}
-
-				if (i == 0) { // si on est sur la première ligne, on met des
-								// lettres
+				
+				// Première ligne : on liste les lettres
+				if(i == 0 && j < this.taille) {
 					map += c + " ";
 					c++;
 				}
-
-				if (j >= this.taille - 1)
-					map += "\n"; // retour à la ligne en fin de ligne.
-
+				else if(i == 0 && j >= this.taille) {
+					map += "\n";	// on retourne à la ligne à la fin de la liste de lettres
+				}
+				
+				
 				if (i > 0 && j > 0) {
 					// TODO : metttre un . c'est vide, # pour un bout de navire,
 					// X pour un tir qui a touché, O pour un tir dans l'eau
@@ -68,6 +71,10 @@ public class GrilleNavale {
 					// # et les emplacements vides .
 					// TODO : ensuite parcours TirReçu pour placer les tirs dans
 					// l'eau O ou sur navire X
+					if(j < this.taille)
+						map += ". ";
+					else
+						map += ".\n";
 				}
 			}
 		}
@@ -79,13 +86,27 @@ public class GrilleNavale {
 		for (int i = 0; i < navires.length; i++)
 			if (n.touche(navires[i]) || n.chevauche(navires[i]))
 				canAdd = false;
+			else
+				nbNavires++;
 		return canAdd;
 	}
 
 	public void placementAuto(int[] taillesNavires) {
 	}
 
-	// private boolean estDansGrille(Coordonnee c) {}
+	private boolean estDansGrille(Coordonnee c) {
+		if (c.getColonne() <= this.taille && c.getLigne() <= this.taille)
+            return true;
+		return false;
+	}
+	
+	public Coordonnee[] getTirsRecus() { // Debug
+        return this.tirsRecus;
+	}
+
+	public void setTirsRecus(Coordonnee[] tirs) {	// Debug
+        this.tirsRecus = tirs;
+	}
 
 	private boolean estDansTirsRecus(Coordonnee c) {
 		for (int i = 0; i < this.tirsRecus.length; i++)
@@ -111,8 +132,10 @@ public class GrilleNavale {
 
 	//// Main pour terster les différentes méthodes ////
 	public static void main(String[] args) {
+		Coordonnee c = new Coordonnee(4, 4);
 		GrilleNavale plateau = new GrilleNavale(8);
 		System.out.println(plateau.toString());
+		plateau.ajouteDansTirsRecus(c);
 	}
 
 }
