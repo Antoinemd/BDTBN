7cdfe5e5ed32e6438a6d5ab6a0319924ce595c93
Hiyuu <Hiyuu@Ecks>
2016-12-15 15:20:45 +0100

    15/12/2016 15h20

---
+++ src/batailleNavale/JoueurAuto.java
@@ -0,0 +1,34 @@
+package batailleNavale;
+
+public class JoueurAuto extends Joueur {
+
+	public JoueurAuto(GrilleNavale g, String nom) {
+		
+	}
+	
+	protected void perdu() {
+		;
+	}
+	
+	protected void gagne() {
+		;
+	}
+	
+	protected void retourAttaque(Coordonnee c, int etat) {
+		;
+	}
+	
+	protected void retourDefense(Coordonnee c, int etat) {
+		;
+	}
+	
+	public void debutAttaque() {
+		;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/batailleNavale/JoueurTexte.java
+++ src/batailleNavale/JoueurTexte.java
@@ -14,23 +14,22 @@ public class JoueurTexte extends Joueur {
 	}
 	
 	protected void perdu() {
-		this.perdu();
 		System.out.println("GAME OVER");
 	}
 	
 	protected void gagne() {
-		this.gagne();
 		System.out.println("YOU WIN");
 	}
 	
 	protected void retourAttaque(Coordonnee c, int etat) {
-		this.retourAttaque(c, etat);
 		System.out.println("Votre tir en " + c + " est " + etat);
+		// Update Grid
+		System.out.println("Grille Attaquant : \n" + this.getGrille());
 	}
 	
 	protected void retourDefense(Coordonnee c, int etat) {
-		this.retourDefense(c, etat);
 		System.out.println("Le tir de votre adversaire en " + c + " est " + etat);
+		System.out.println("Grille Attaqué : \n" + this.getGrille());
 	}
 	
 	public void debutAttaque(){
@@ -44,7 +43,7 @@ public class JoueurTexte extends Joueur {
 	}
 
 	public static void main(String[] args) {
-		int[] tN = {2, 3, 3, 4, 5};
+		int[] tN = {1};
 		GrilleNavale g1 = new GrilleNavale(10, tN);
 		g1.placementAuto(tN);
 		System.out.println("Grille joueur 1 : \n" + g1 + "\n");
@@ -54,10 +53,9 @@ public class JoueurTexte extends Joueur {
 		System.out.println("Grille joueur 2 : \n" + g2 + "\n");
 		Joueur j1 = new JoueurTexte(g1, "Kris");
 		Joueur j2 = new JoueurTexte(g2, "Mik");
+		j1.jouerAvec(j2);
 		
 		j1.debutAttaque();
-		g1.placementAuto(tN);
-		g2.placementAuto(tN);
 	}
 
 }
