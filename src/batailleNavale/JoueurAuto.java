a221267f5270a8a8799b72c9886477f13dab42c9
Hiyuu <Hiyuu@130.190.7.250>
2016-12-16 13:14:07 +0100

    // Màj 16/12/16 à 13:13 (IA réparée, mais pas optimale)

--- src/batailleNavale/JoueurAuto.java
+++ src/batailleNavale/JoueurAuto.java
@@ -28,67 +28,11 @@ public class JoueurAuto extends Joueur {
 	
 	public void debutAttaque() {
 		System.out.println("A votre tour d'attaquer !" + this.getNom());
-		Coordonnee c = new Coordonnee(0, 0);	// Coordonnees invalides tant qu'elles ne sont pas modifiées
-		
-		// Gestion du premier tir (entièrement aléatoire)
-		if (super.getGrilleAdversaire().getTableauTirsRecus().length < 1) { // On vérifie si on a déjà tiré sur l'adversaire
+		Coordonnee c = new Coordonnee(0, 0);
 			do {
 				c = new Coordonnee((int)(Math.random() * (super.getGrille().getTailleGrille())) + 1, (int)(Math.random() * (super.getGrille().getTailleGrille())) + 1);
 			} while(super.getGrilleAdversaire().getEstDansTirsRecus(c));
 				this.attaque(c);
-		}
-//		// Gestion d'un tir où 'c' est dans l'eau
-//		for (int i = 0; i < super.getGrilleAdversaire().getTableauTirsRecus().length; i++) {
-//			if (super.getGrilleAdversaire().getTableauTirsRecus()[i] == c){
-//				if (super.getGrilleAdversaire().estALEau(c) || super.getGrilleAdversaire().estCoule(c))
-//					this.tirDeRecherche(c);
-//					else {
-//						this.tirDeDestruction(c);
-//					}
-//				
-//				this.attaque(c);
-//			}
-//		}
-//	}
-//	
-//	public Coordonnee tirDeRecherche(Coordonnee c) {
-//		do {
-//			c = new Coordonnee((int)(Math.random() * (super.getGrille().getTailleGrille())) + 1, (int)(Math.random() * (super.getGrille().getTailleGrille())) + 1);
-//		} while(super.getGrilleAdversaire().getEstDansTirsRecus(c));
-//		return c;
-//	}
-//	
-//	public Coordonnee tirDeDestruction(Coordonnee c) {
-//		do {
-//			for (int i = 0; i < super.getGrilleAdversaire().getTableauTirsRecus().length; i++) {
-//				if(super.getGrilleAdversaire().getTableauTirsRecus()[i].voisine(c)	// s'il existe un tir voisin à d
-//						&& super.getGrilleAdversaire().estTouche(super.getGrilleAdversaire().getTableauTirsRecus()[i])	// que ce tir touche un navire
-//						&& super.getGrilleAdversaire().getTableauTirsRecus()[i].getLigne() == c.getLigne() - 1) {	// et qu'il est sur la ligne d'avant
-//					c = new Coordonnee(c.getLigne() + 1, c.getColonne());	// on attaque la ligne suivante
-//					break;
-//				}
-//				else if(super.getGrilleAdversaire().getTableauTirsRecus()[i].voisine(c)
-//						&& super.getGrilleAdversaire().estTouche(super.getGrilleAdversaire().getTableauTirsRecus()[i])
-//						&& super.getGrilleAdversaire().getTableauTirsRecus()[i].getLigne() == c.getLigne() + 1) {
-//					c = new Coordonnee(c.getLigne() - 1, c.getColonne());
-//					break;
-//				}
-//				else if(super.getGrilleAdversaire().getTableauTirsRecus()[i].voisine(c)
-//						&& super.getGrilleAdversaire().estTouche(super.getGrilleAdversaire().getTableauTirsRecus()[i])
-//						&& super.getGrilleAdversaire().getTableauTirsRecus()[i].getColonne() == c.getColonne() - 1) {
-//					c = new Coordonnee(c.getLigne(), c.getColonne() + 1);
-//					break;
-//				}
-//				else if(super.getGrilleAdversaire().getTableauTirsRecus()[i].voisine(c)
-//						&& super.getGrilleAdversaire().estTouche(super.getGrilleAdversaire().getTableauTirsRecus()[i])
-//						&& super.getGrilleAdversaire().getTableauTirsRecus()[i].getColonne() == c.getColonne() + 1) {
-//					c = new Coordonnee(c.getLigne(), c.getColonne() - 1);
-//					break;
-//				}
-//				
-//			}
-//		} while(super.getGrilleAdversaire().getEstDansTirsRecus(c));
-//		return c;
 	}
 
 	public static void main(String[] args) {
